e903f6633f2be27ba4568619e96c2ce7315842ba
Sean Shields <sean@dmgctrl.com>
2014-09-28 22:51:35 -0400

    cleanup

--- js/btnDown.js
+++ js/btnDown.js
@@ -3,7 +3,6 @@ $(function() {
     $('.next').on('click', function(){
         $('section').each(function() {
             var pos = $(this).offset().top;  
-            console.log("pos: " + pos, "$window.scrollTop(): " + $window.scrollTop());
             if ($window.scrollTop() < pos) {
                 $('html, body').animate({
                     scrollTop: pos
