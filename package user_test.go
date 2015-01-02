0bd5263552e1109c8aedd3f6f7e22eaf7f7b134e
Sean Shields <sean@dmgctrl.com>
2015-01-02 09:50:35 -0500

    first go test

---
+++ package user_test.go
@@ -0,0 +1,26 @@
+package user_test
+
+import (
+    . "github.com/onsi/ginkgo"
+    . "github.com/onsi/gomega"
+    "user"
+)
+
+var _ = Describe("User", func() {
+    var u *user.User
+
+    BeforeEach(func() {
+        var err error
+        u, err = user.New()
+        Expect(err).NotTo(HaveOccurred())
+    })
+
+    Describe("Password validation", func() {
+        Context("Password should be valid", func() {
+            It("should be entered as pacia1986", func() {
+                u.password = "pacia1986"
+                Expect(u.password()).To(Equal("pacia1986"))
+            })
+        })
+    })
+})
